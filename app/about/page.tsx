d3817248107f3cc49493a38a1b7df25c45f5ba4d
YUGA <uga.toyoda0305@gmail.com>
2026-03-07 23:42:33 +0900

    初期整理を行ったコミット

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header"
+
+export default function About() {
+    return (
+    <>
+        <Header />
+        <main>
+            <h1>About Page</h1>
+            <h1>dji</h1>
+        </main>
+        <Footer />
+    </>
+    );
+}
---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="h-16 bg-gray-800 text-white text-xs flex justify-center items-center">
+            © 2026 豊田優雅.
+        </footer>
+    );
+}
