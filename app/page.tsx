51b891471edc6d3c4709926cb1249fd41a1f1025
YUGA <uga.toyoda0305@gmail.com>
2026-03-08 02:05:04 +0900

    メインページにスライダーを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Slider from "@/components/Slider";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Slider />
       <Footer />
     </>
   );
