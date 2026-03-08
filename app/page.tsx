029a7d3183903b9530180d3bb686daa3f62ed094
YUGA <uga.toyoda0305@gmail.com>
2026-03-09 00:17:32 +0900

    不要なファイルの削除、文法等の修正、サイドバーの追加を行いました。

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Slider from "@/components/Slider";
+import SideBar from "@/components/SideBar";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <SideBar />
       <Slider />
       <Footer />
     </>
--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import Image from "next/image";
 import { fruits } from "./data";
 
 export default function Slider() {
@@ -32,26 +33,44 @@ export default function Slider() {
     return (
         <div className="text-center">
             <h1 className="font-bold text-4xl">{fruits[index].name}</h1>
-            <img src={`/images/${fruits[index].image}`} alt={fruits[index].name} className="h-64 mx-auto" />
+            <Image
+                src={`/images/${fruits[index].image}`}
+                alt={fruits[index].name}
+                className="h-64 mx-auto"
+                height={256}
+            />
             <p>Page {index + 1} of {fruits.length}</p>
-            <button onClick={prevClick} className="bg-cyan-700 hover:bg-cyan-950 mr-4 px-4 py-1 text-white rounded disabled:bg-gray-400">
+
+            <button
+                onClick={prevClick}
+                className="bg-cyan-700 hover:bg-cyan-950 mr-4 px-4 py-1 text-white rounded disabled:bg-gray-400"
+            >
                 Prev
             </button>
-            
-            <button onClick={nextClick} className="bg-cyan-700 hover:bg-cyan-950 ml-30 px-4 py-1 text-white rounded disabled:bg-gray-400">
+
+            <button
+                onClick={nextClick}
+                className="bg-cyan-700 hover:bg-cyan-950 ml-30 px-4 py-1 text-white rounded disabled:bg-gray-400"
+            >
                 Next
             </button>
             <br />
-            <button onClick={showMoreClick} className="bg-green-500 hover:bg-green-700 text-white py-1 px-4 rounded mt-5 mb-3">
+
+            <button
+                onClick={showMoreClick}
+                className="bg-green-500 hover:bg-green-700 text-white py-1 px-4 rounded mt-5 mb-3"
+            >
                 {showMore ? "Show Less" : "Show More"}
             </button>
+
             {showMore && (
                 <ul className="text-left max-w-md mx-auto mb-5 bg-gray-200 p-4 rounded">
                     <li>学名: {fruits[index].scientificName}</li>
                     <li>目　: {fruits[index].order}</li>
                     <li>科　: {fruits[index].family}</li>
                     <li>属　: {fruits[index].genus}</li>
-                </ul>)}
+                </ul>
+            )}
         </div>
     );
 
